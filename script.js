0a74be1feb0032921cf112e7141ba2bc510aa327
Ankit Jaiswal <ankitjaiswal3016@gmail.com>
2025-09-16 06:53:08 +0530

    page 1 complete

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+function loadingAnimation(){
 var tl = gsap.timeline()
 tl.from(".line h1",{
     y:150,
@@ -5,7 +6,7 @@ tl.from(".line h1",{
     duration:0.6,
     delay:0.5
 })
-tl.from("#line1-part1,.line h2",{
+tl.from("#line1-part1",{
     opacity:0,
     onStart:function(){
          var h5timer = document.querySelector("#line1-part1 h5")
@@ -16,12 +17,50 @@ tl.from("#line1-part1,.line h2",{
         }else {
             h5timer.innerHTML = grow
         }
-    },30)
-
+    },33)
     }
+})
+tl.to(".line h2",{
+    animationName:"anime",
+    opacity:1,
 })
 tl.to("#loader",{
     opacity: 0 ,
-    duration : 0.4,
+    duration : 0.2,
     delay:4,
 })
+tl.from("#page1",{
+    delay:0.2,
+    opacity: 0 ,
+    y:1600,
+    opacity:0,
+    duration:0.6,
+    ease:"power4",
+})
+
+tl.to("#loader",{
+    display:"none"
+})
+tl.from("#nav",{
+    opacity:0
+})
+tl.from("#pg-con1 h1,#pg-con2 h1,#pg-con3 h2 ,#pg-con4 h1",{
+    y:120,
+    stagger:0.2
+})
+}
+
+
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(dets){
+    gsap.to("#crsr",{
+        left:dets.x,
+        top:dets.y,
+    })
+})
+Shery.makeMagnet("#nav-part2 h4 ");
+}
+
+loadingAnimation()
+
+cursorAnimation()
